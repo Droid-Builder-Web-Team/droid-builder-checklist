795acf0f572547f91ea136ddcc3b301d52492358
Rob Howdle <robhowdle94@gmail.com>
2023-06-06 19:36:46 +0100

    Adjusted share query to pull through the user and the role for that user

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Middleware;
+use Silber\Bouncer\Bouncer;
 use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
@@ -28,17 +30,45 @@ class HandleInertiaRequests extends Middleware
      *
      * @return array<string, mixed>
      */
+//    public function share(Request $request): array
+//    {
+//        return array_merge(parent::share($request), [
+//            'auth' => [
+//                'user' => $request->user(),
+//            ],
+//            'ziggy' => function () use ($request) {
+//                return array_merge((new Ziggy)->toArray(), [
+//                    'location' => $request->url(),
+//                ]);
+//            },
+//        ]);
+//    }
+
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
-                    'location' => $request->url(),
-                ]);
-            },
-        ]);
+        $sharedData = parent::share($request);
+
+        $user = $request->user();
+        $bouncer = app(Bouncer::class);
+        $roleId = DB::table('assigned_roles')
+            ->where('entity_id', $user->id)
+            ->value('role_id');
+
+        $role = $bouncer->role()->where('id', $roleId)->first();
+
+        $sharedData['auth']['user'] = [
+            'id' => optional($user)->id,
+            'name' => optional($user)->name,
+            'email' => optional($user)->email,
+            'role' => $role,
+        ];
+
+        $sharedData['ziggy'] = function () use ($request) {
+            return array_merge((new Ziggy)->toArray(), [
+                'location' => $request->url(),
+            ]);
+        };
+
+        return $sharedData;
     }
 }
