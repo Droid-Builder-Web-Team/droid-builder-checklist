7475efb218e31624c1243e34b29665687deec362
Rob Howdle <robhowdle94@gmail.com>
2023-06-08 12:44:54 +0100

    Dashboard greeting message component

---
+++ resources/js/Components/GreetingMessage.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    computed: {
+        greeting() {
+            const currentTime = new Date();
+            const hour = currentTime.getHours();
+            let greeting = '';
+
+            if (hour >= 5 && hour < 12) {
+                greeting = 'Good Morning';
+            } else if (hour >= 12 && hour < 18) {
+                greeting = 'Good Afternoon';
+            } else {
+                greeting = 'Good Evening';
+            }
+
+            return greeting;
+        }
+    }
+}
+</script>
+
+<template>
+    {{ greeting }},
+</template>
+
+<style scoped>
+
+</style>
