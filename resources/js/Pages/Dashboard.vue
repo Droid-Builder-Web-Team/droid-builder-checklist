71255bbd69160636ef9d3b11ec5709b1b1abf90f
Rob Howdle <robhowdle94@gmail.com>
2023-06-08 12:46:36 +0100

    Updated to add greeting message and quoteapi components

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
+import GreetingMessage from '@/Components/GreetingMessage.vue';
+import QuotesAPI from "@/Components/QuotesAPI.vue";
 </script>
 
 <template>
@@ -12,11 +14,64 @@ import {Head} from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+            <div class="max-w-7xl mx-auto sm:px-2 lg:px-4">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-center">
+                    <div class="pt-6 text-2xl text-gray-900 dark:text-gray-100">
+                        <GreetingMessage/>
+                        {{ $page.props.auth.user.name }}.
+                    </div>
+
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <QuotesAPI/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-4 gap-2">
+            <div>
+                <div class="max-w-7xl mx-auto sm:px-2 lg:px-4">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 w-100 flex flex-1 flex-col gap-2 text-gray-900 dark:text-gray-100 text-base">
+                            <p class="uppercase">Active Builds</p>
+                            <span class="text-xl">10</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div class="max-w-7xl mx-auto sm:px-2 lg:px-4">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 w-100 flex flex-1 flex-col gap-2 text-gray-900 dark:text-gray-100 text-base">
+                            <p class="uppercase">Completed Builds</p>
+                            <span class="text-xl">5</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row-span-3 col-span-2">
+                <div class="max-w-7xl mx-auto sm:px-2 lg:px-4">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100 text-base flex flex-1 gap-2 flex-col">
+                            <p class="uppercase">Notifications</p>
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+
--- resources/js/app.js
+++ resources/js/app.js
@@ -21,3 +21,35 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+// Check if the user prefers dark mode
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Get the root HTML element
+const root = document.documentElement;
+
+// Function to enable or disable dark mode
+function enableDarkMode() {
+    root.classList.add('dark');
+}
+
+function disableDarkMode() {
+    root.classList.remove('dark');
+}
+
+// Set the initial dark mode state based on user preference
+if (prefersDarkMode) {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+// Listen for changes in user preference
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    if (e.matches) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
+
