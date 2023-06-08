6aa68e41b1a11708d9d1c78f92be9f5ba73e7286
Rob Howdle <robhowdle94@gmail.com>
2023-06-08 12:45:13 +0100

    Quote API to retrieve random inspiration quotes to display on the users dashboard

---
+++ resources/js/Components/QuotesAPI.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-if="quote" class="flex justify-center gap-4">
+        <i class="fa-solid fa-quote-left"></i>
+        <p><span class="italic">{{ quote.text }}</span> - {{ quote.author }}</p>
+        <i class="fa-solid fa-quote-right"></i>
+    </div>
+    <div v-else>
+        Loading quote...
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            quote: null,
+        };
+    },
+    mounted() {
+        this.fetchQuote();
+    },
+    methods: {
+        fetchQuote() {
+            fetch("https://type.fit/api/quotes")
+                .then((response) => response.json())
+                .then((data) => {
+                    const randomIndex = Math.floor(Math.random() * data.length);
+                    this.quote = data[randomIndex];
+                    console.log(this.quote);
+                })
+                .catch((error) => {
+                    console.error("Error fetching quote:", error);
+                });
+        },
+    },
+};
+</script>
+
+<style scoped>
+</style>
