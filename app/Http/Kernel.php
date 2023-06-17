5a6bfad39a660ce3da5b5da0d3c2e6b9dc24bc54
Rob Howdle <robhowdle94@gmail.com>
2023-06-17 23:29:14 +0100

    Installed Sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
@@ -65,9 +66,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            ThrottleRequests::class.':api',
+            EnsureFrontendRequestsAreStateful::class,
+            ThrottleRequests::class . ':api',
             SubstituteBindings::class,
+            ThrottleRequests::class . ':api',
+            SubstituteBindings::class,
+
         ],
     ];
 
